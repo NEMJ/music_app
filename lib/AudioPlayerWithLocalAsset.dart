cb65e9376d94a5cef986f5457c7a43005162e657
Nelson Junior <dudu.da.mata@hotmail.com>
2022-05-01 00:41:07 -0300

    slider layout error

--- lib/AudioPlayerWithLocalAsset.dart
+++ lib/AudioPlayerWithLocalAsset.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -16,6 +18,22 @@ class _AudioPlayerWithLocalAssetState extends State<AudioPlayerWithLocalAsset> {
 
   String path = 'A Sky Full of Stars.mp3';
 
+  int timeProgress = 0;
+  int audioDuration = 0;
+
+  Widget slider() {
+    return Container(
+      width: 300,
+      child: Slider.adaptive(
+        value: (timeProgress/1000).floorToDouble(),
+        max: (audioDuration/1000).floorToDouble(),
+        onChanged: (value) {
+          seekToSec(value.toInt());
+        },
+      ),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -24,6 +42,12 @@ class _AudioPlayerWithLocalAssetState extends State<AudioPlayerWithLocalAsset> {
     audioPlayer.onPlayerStateChanged.listen((PlayerState s) {
       setState(() {
         playerState = s;
+      });
+    });
+
+    audioPlayer.onAudioPositionChanged.listen((Duration p) {
+      setState(() {
+        timeProgress = p.inMilliseconds;
       });
     });
   }
@@ -68,10 +92,59 @@ class _AudioPlayerWithLocalAssetState extends State<AudioPlayerWithLocalAsset> {
                     : Icons.play_arrow_rounded
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(getTimeString(timeProgress)),
+                      SizedBox(width: 20),
+                      slider(),
+                      SizedBox(width: 20),
+                      audioDuration == 0
+                        ? getFileAudioDuration()
+                        : Text(getTimeString(audioDuration)),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
         ),
       );
+  }
+
+  Widget getFileAudioDuration() {
+    return FutureBuilder(
+      future: _getAudioDuration(),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<int> snapshot) {
+          if(snapshot.connectionState == ConnectionState.done) {
+            return Text(getTimeString(snapshot.data!));
+          }
+          return Text('');
+        });
+  }
+
+  Future<int> _getAudioDuration() async {
+    File audioFile = await audioCache.load(path) as File;
+    await audioPlayer.setUrl(audioFile.path);
+    audioDuration = await Future.delayed(Duration(seconds: 2), () => audioPlayer.getDuration());
+    return audioDuration;
+  }
+
+  String getTimeString(int milliseconds) {
+    if(milliseconds == null) milliseconds = 0;
+    String minutes =
+      '${(milliseconds / 60000).floor() < 10 ? 0 : ''}${(milliseconds / 60000).floor()}';
+    String seconds = 
+      '${(milliseconds / 1000).floor() % 60 < 10 ? 0 : ''}${(milliseconds / 1000).floor() % 60}';
+    return '$minutes:$seconds';
+  }
+
+  void seekToSec(int sec) {
+    Duration newPosition = Duration(seconds: sec);
+    audioPlayer.seek(newPosition);
   }
 }
